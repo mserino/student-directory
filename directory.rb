18c69091c624e509ca4f885cd2eec8c8f3d1e7d1
Margherita <m.serino@hotmail.it>
2014-05-13 14:07:52 +0100

    interpolating strings

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+student_count = 9
 # first we print the list of students
 puts "The students of my cohort at Makers Academy"
 puts "-------------"
@@ -21,7 +22,4 @@ puts "Nicolai DTH"
 puts "Nadav Matalon"
 puts "Fitsum Teklehaimanot"
 # finally we print the total
-print "Overall we have "
-#it's important that print() doesn't add new line characters
-print 9
-puts " great students"
+puts "Overall we have #{student_count} great students!"
