e6cf842045f4f219a389552584015e1595bf747a
Margherita <m.serino@hotmail.it>
2014-05-13 15:31:44 +0100

    hashes

--- directory.rb
+++ directory.rb
@@ -1,32 +1,30 @@
 # Let's put all students into an array!
 students = [
-	 "Roi Driscoll",
-	 "James Whyte",
-	 "Margherita Serino",
-	 "Vaidas Mykolaitis",
-	 "Johann Bornman",
-	 "Kate Hamilton",
-	 "James Keap",
-	 "Nic Yeeles",
-	 "Julie Walker",
-	 "Will Allen",
-	 "Julia Tan",
-	 "Federico Maffei",
-	 "Jamie Patel",
-	 "Faezrah Rizalman",
-	 "Josh Fail-Broon",
-	 "Sasha Cooper",
-	 "Nicolai DTH",
-	 "Nadav Matalon",
-	 "Fitsum Teklehaimanot"
-	]
-#and then print them
-puts "The students of my cohort at Makers Academy"
-puts "-------------------"
+	 {:name => "Roi Driscoll", :cohort => :january},
+	 {:name => "James Whyte", :cohort => :march},
+	 {:name => "Margherita Serino", :cohort => :december},
+	 {:name => "Vaidas Mykolaitis", :cohort => :march},
+	 {:name => "Johann Bornman", :cohort => :january},
+	 {:name => "Kate Hamilton", :cohort => :december},
+	 {:name => "James Keap", :cohort => :march},
+	 {:name => "Nic Yeeles", :cohort => :march}
+]
 
-students.each do |student|
-	puts student
+def print_header
+  puts "The students of my cohort at Makers Academy"
+  puts "-------------"
 end
 
-#finally, we print the total
-puts "Overall, we have #{students.length} great students!"
+def print(students)
+	students.each do |student|
+		puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.length} great students"
+end 
+#nothing happens until we call the methods
+print_header
+print(students)
+print_footer(students)
