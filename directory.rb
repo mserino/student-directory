518880d079bce91c08f177c6f36ae2bb88d81155
Margherita <m.serino@hotmail.it>
2014-05-14 14:20:47 +0100

    added load_students

--- directory.rb
+++ directory.rb
@@ -34,7 +34,8 @@ end
 def print_menu
 	puts "1. Input the students"
 	puts "2. Show the students"
-	puts "3. Save"
+	puts "3. Save the list to students.csv"
+	puts "4. Load the list from students.csv"
 	puts "9. Exit"
 end
 
@@ -52,6 +53,8 @@ def process(selection)
 		show_students
 	when "3"
 		save_students
+	when "4"
+		load_students
 	when "9"
 		exit
 	else
@@ -61,7 +64,7 @@ end
 
 def save_students
   # open the file for writing
-  file = File.open("students.csv", "a+")
+  file = File.open("students.csv", "w")
   # iterate over the array of students
   @students.each do |student|
     student_data = [student[:name], student[:cohort]]
@@ -69,8 +72,19 @@ def save_students
     file.puts csv_line
   end
   file.close
+  puts "File saved."
 end
 
+def load_students
+	#open the file saved before
+	file = File.open("students.csv", "r")
+	file.readlines.each do |line|
+		name, cohort = line.chomp.split(', ')
+		@students << {:name => name, :cohort => cohort.to_sym}
+	end
+	file.close
+	puts "\"students.csv\" loaded."
+end
 
 def interactive_menu
 	loop do
